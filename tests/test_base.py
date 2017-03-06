6e495c06fdbf2b83e30139f5ed2349296a2bf77f
Jeongsoo Park <toracle@gmail.com>
2017-03-06 05:31:58 -0800

    add a fixture

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,2 +1,16 @@
-def test_aa():
-    pass
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+from bankocr import Ocr
+
+
+'''
+    _  _     _  _  _  _  _
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _| 
+
+'''
+
+def fixture_one():
+    return '   \n  |\n  |\n   \n'
